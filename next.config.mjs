1d018300f9136f907b40560c63c24851c8303f40
divyanshinvester <divyanshmishra.investor@gmail.com>
2026-08-20 23:48:04 +0530

    core done gamil rectoin

--- next.config.mjs
+++ next.config.mjs
@@ -26,6 +26,7 @@ const nextConfig = {
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "http://localhost:3000" },
+          { key: "Access-Control-Allow-Origin", value: "https://receptionrantraa.vercel.app" },
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
           { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization, X-Requested-With" },
         ],
